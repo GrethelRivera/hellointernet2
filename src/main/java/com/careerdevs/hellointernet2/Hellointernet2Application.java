1e2dc88abab315b2d9e162698cacac82d318384d
GrethelRivera <grethelr91@gmail.com>
2022-08-06 18:14:52 -0400

    Initial commit

---
+++ src/main/java/com/careerdevs/hellointernet2/Hellointernet2Application.java
@@ -0,0 +1,13 @@
+package com.careerdevs.hellointernet2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hellointernet2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hellointernet2Application.class, args);
+	}
+
+}
