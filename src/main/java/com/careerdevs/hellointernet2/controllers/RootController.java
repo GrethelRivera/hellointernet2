4315be4cda93300d2ce5fd1fb56bd89576d1615f
GrethelRivera <grethelr91@gmail.com>
2022-08-06 20:43:14 -0400

    First Commit - 1/3 - Complete

---
+++ src/main/java/com/careerdevs/hellointernet2/controllers/RootController.java
@@ -0,0 +1,25 @@
+package com.careerdevs.hellointernet2.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    private String routeRoute() {
+        return "You requested the root route";
+    }
+
+    @GetMapping("/hello")
+    private String helloRoute() {
+        return "Hello CareerDevs!!!!";
+    }
+
+    @GetMapping ("/random")
+    private int randomRoute () {
+        return (int) (Math.random() * 100) + 1;
+    }
+}
+
+
