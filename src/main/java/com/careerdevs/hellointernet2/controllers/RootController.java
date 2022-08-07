206141d74642cd357497a9afd0472b04210f4276
GrethelRivera <grethelr91@gmail.com>
2022-08-07 13:03:21 -0400

    Third Commit - 3/3 COMPLETE

--- src/main/java/com/careerdevs/hellointernet2/controllers/RootController.java
+++ src/main/java/com/careerdevs/hellointernet2/controllers/RootController.java
@@ -20,6 +20,19 @@ public class RootController {
     private int randomRoute () {
         return (int) (Math.random() * 100) + 1;
     }
+
+    @GetMapping ("/joke")
+    private String jokeRoute () {
+        String [] jokes = {
+                "What do you call a pig that does karate? A pork chop.",
+                "Why do people say “break a leg” when you go on stage? Because every play has a cast.",
+                "What do you call an alligator detective? An investi-gator.",
+                "An SQL query goes into a bar, walks up to two tables, and asks, “Can I join you?”"
+        };
+int jokeIndex = (int) (Math.random() * jokes.length);
+return jokes[jokeIndex];
+
+    }
 }
 
 
